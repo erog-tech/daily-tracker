fca4e6dd1e934a4b67a5f0fe4e34d10a1597fe43
erog-tech <prikshitthakur0@gmail.com>
2022-07-16 19:14:30 +0200

    test the openapi

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,22 @@ const fetchNASAData = async () => {
   }
   
   fetchNASAData()
+
+const urlWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=52.52&lon=13.405&exclude=hourly,daily&units=metric&appid='
+const api_key_2 = '3471f6859c5ebc2c8c031f9ab7932105'
+
+const fetchWeatherData = async () => {
+    try {
+      const response = await fetch(`${urlWeather}${api_key_2}`)
+      const data = await response.json()
+      console.log('Weather data', data)
+      displayData_2(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const displayData_2 = data => {
+    document.getElementById('temp').textContent = data.timezone + ': ' + data.current.temp + ' °C ' 
+  }
+  
+  fetchWeatherData()
