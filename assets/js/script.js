db07fcb30a523d3b31f8c30b798d9967bddcf758
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-24 11:22:46 +0200

    Improve visual styling of scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ function showDate() {
   $('#todayDate').html(dt.toLocaleString(DateTime.DATE_HUGE));
 }
 
+// Use "now-" and currentHour value as a class for the body element to use css selectors for color
+function highlightCurrentHour() {
+  let currentHour = DateTime.local().hour; // set the current hour
+  $("#current-day").html(dt.toLocaleString(DateTime.DATE_HUGE)); // show the current date
+  $("body").addClass(`now-${currentHour}`); 
+   }
+
 setInterval(showDate, 1000); // call for the current hour every second
 
 
